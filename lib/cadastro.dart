4bb5f3950c3ef25a1897e785520b352bafe248e7
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-09-08 22:28:39 -0300

    Alteração de msg

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -91,7 +91,7 @@ class CadastroScreenState extends State<CadastroScreen> {
         return responseData['codRetorno'] == 200 &&
             responseData['message'] == 'OK';
       } else {
-        _showErrorDialog('Erro ao verificar CPF. Tente novamente.');
+        _showErrorDialog(responseData['message']);
         return false;
       }
     } catch (e) {
