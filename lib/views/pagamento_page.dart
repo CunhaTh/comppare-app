dfe24372ec4f146a79dd7e668295e62b76d60103
unknown <contatodev.thiago@gmail.com>
2025-09-10 17:57:56 -0300

    atualiza text Planos

--- lib/views/pagamento_page.dart
+++ lib/views/pagamento_page.dart
@@ -268,7 +268,7 @@ class _PagamentoPageState extends State<PagamentoPage>
                   _buildFeatureChip(
                       '🏷️ ${widget.plano.quantidadeTags} categorias'),
                   _buildFeatureChip(
-                      '📁 ${widget.plano.quantidadePastas} pastas'),
+                      '''📁 ${widget.plano.quantidadePastas} ''' + (widget.plano.nome.toLowerCase().contains('gratuito') ? 'Álbum' : 'Álbuns'),),
                  /* _buildFeatureChip(
                       '👥 ${widget.plano.quantidadeConvites} convites'),*/
                 ],
